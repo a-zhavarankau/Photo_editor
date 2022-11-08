d34419347ab0c07befd8db3eeb38bac27d70eceb
a.zhavarankau <a.zhavarankau@gmail.com>
2022-11-08 18:41:35 +0100

    Refactor 'crop_bars_on_screenshots.py'

--- crop_bars_on_screenshots/ReadMe.md
+++ crop_bars_on_screenshots/ReadMe.md
@@ -1,6 +1,7 @@
 ## Crop bars on screenshots
 
 Crop redundant upper and lower bars on screenshots. Adjusted to the Macbook resolution 2880x1800.
+If image is not screenshot (*.png file with 2880x1800) and it's resolution less 2500x2000 than script resize this file.
 
 ### Instruments:
 - PIL (Python Image Library)
--- crop_bars_on_screenshots/crop_bars_on_screenshots.py
+++ crop_bars_on_screenshots/crop_bars_on_screenshots.py
@@ -18,8 +18,28 @@ def get_file_name_and_ext(file) -> tuple[str, str] or None:
         return None
     return file_name, file_ext
 
+def crop_image(img: Image, width: int, height: int) -> Image:
+    left = 0  # Coordinates of the rectangular image that will survive after cropping
+    top = 210
+    right = width
+    bottom = height - 170
 
-def edit_save_images(source_folder):
+    img_cropped = img.crop((left, top, right, bottom))
+    img_edited = img_cropped.resize((2500, img_cropped.size[1] * 2500 // width))
+    # img_cropped.show()    # Show cropped image in the image viewer (to check the result)
+    return img_edited
+
+def resize_image(img: Image, width: int, height: int) -> Image:
+    img_edited = img.resize((width * 1800 // height, 1800))
+    return img_edited
+
+def save_img(img_edited, dest_folder, file_name, file_ext):
+    img_edited.save(f"{dest_folder}/{file_name}.{file_ext}")  # Edited file name == source file name
+    # img_edited.save(f"{dest_folder}/{count}.{file_ext}")     # Edited file name == count
+    return img_edited
+
+
+def save_images(source_folder):
     with os.scandir(source_folder) as folder_content:
         list_folder_content = list(folder_content)
         files_amount = len(list_folder_content)
@@ -29,7 +49,7 @@ def edit_save_images(source_folder):
                 file = entity
                 try:
                     file_name, file_ext = get_file_name_and_ext(file)
-                except BaseException:
+                except Exception:
                     print(f"Something wrong with {file.name}")
                     pass
                 else:
@@ -38,24 +58,17 @@ def edit_save_images(source_folder):
 
                     img = Image.open(file_path)
                     width, height = img.size
-                    if file_ext.lower() == 'png' and width == 2880 and height == 1800:  # Check if the file a screenshot
-                        left = 0                 # Coordinates of the rectangular image that will survive after cropping
-                        top = 210
-                        right = width
-                        bottom = height - 170
-
-                        img_cropped = img.crop((left, top, right, bottom))
-                        img_edited = img_cropped.resize((2500, img_cropped.size[1] * 2500 // width))
-                        # img_cropped.show()    # Show cropped image in the image viewer (to check the result)
+                    if file_ext.lower() == 'png' and width == 2880 and height == 1800:  # Check if file a screenshot
+                        img_edited = crop_image(img, width, height)
                     elif all((file_ext.lower() in ['jpg', 'jpeg'], width > 2500, height > 2000)):
-                        img_edited = img.resize((width * 1800 // height, 1800))
-
-                    img_edited.save(f"{dest_folder}/{file_name}.{file_ext}")   # Edited file name == source file name
-                    # img_edited.save(f"{dest_folder}/{count}.{file_ext}")     # Edited file name == count
+                        img_edited = resize_image(img, width, height)
+                    else:
+                        continue
+                    save_img(img_edited, dest_folder, file_name, file_ext)
                     print(f"[INFO] File {count} of {files_amount} ready")
                     count += 1
 
 
 source_folder = "/Users/Sasha/Documents/Python/Тестирования/Python_Proghub_dev"
-edit_save_images(source_folder)
+save_images(source_folder)
 
