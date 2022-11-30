4c98b72b98d88f35153693210d6d1f148856e8e5
a.zhavarankau <a.zhavarankau@gmail.com>
2022-11-30 22:34:43 +0100

    Refactor 'crop_2.py' and 'plan.txt'

--- crop_bars_on_screenshots/crop_2.py
+++ crop_bars_on_screenshots/crop_2.py
@@ -1,10 +1,11 @@
 import os
 from PIL import Image, UnidentifiedImageError
 from working_files.constants import *
-# from typing import Any
 
 
 def create_dest_folder(init_folder: str) -> str:
+    """Create destination folder named as source folder + '_cropped'.
+       If destination folder exists already, skip this error."""
     dest_folder = init_folder + "_cropped"
     try:
         os.mkdir(dest_folder)
@@ -33,32 +34,38 @@ def get_file_name_and_ext(file: any) -> tuple[str] or False:
 
 
 def check_is_img(file_path: str, file_ext: str) -> any:
+    """Check if the file is a proper image"""
     if file_ext in ['bmp', 'jpeg', 'jpg', 'png']:
         image = Image.open(file_path)
         return image
-    raise AttributeError(f"unknown extension to handle: \'{file_ext}\'")
+    raise AttributeError(f"unknown extension to process: \'{file_ext}\'")
 
 
 def check_is_screenshot(width: int, height: int, file_ext: str) -> bool:
-    return ((width, height) == (SCREEN_WIDTH, SCREEN_HEIGHT)) and file_ext.lower() == 'png'
+    return ((width, height) == (SCREEN_WIDTH, SCREEN_HEIGHT)) \
+           and file_ext.lower() == 'png'
 
 
-def crop_bars(image: Image):
-    left = 0  # Coordinates of the rectangular image that will survive after cropping
+def crop_bars(image: Image) -> Image:
+    # Coordinates of the rectangular image that will survive after cropping
+    left = 0
     top = 210
     right = SCREEN_WIDTH
     bottom = SCREEN_HEIGHT - 170
 
     image_cropped = image.crop((left, top, right, bottom))
-    # img_cropped.show()    # Show cropped image in the image viewer (to check the result)
+    # Command below is used to show cropped image (visual check of the result)
+    # image_cropped.show()
     return image_cropped
 
 
-def img_bigger_than_default(width: int, height: int):
+def check_img_bigger_than_default(width: int, height: int) -> bool:
     return width > DEFAULT_WIDTH or height > SCREEN_HEIGHT
 
 
-def resize_img(image: Image, width: int, height: int):
+def resize_img_to_default(image: Image, width: int, height: int) -> Image:
+    """Check if image vertical rectangular and resize it with height=1800;
+       else image is horizontal rectangular and resize it with width=2400"""
     if height > width:
         # If image is vertical rectangular, resize image.height = SCREEN_HEIGHT, image.width -> respectively
         resized_height = SCREEN_HEIGHT
@@ -71,9 +78,9 @@ def resize_img(image: Image, width: int, height: int):
     return resized_img
 
 
-def save_img(image: Image, dest_folder_name, file_name, file_ext, count):
-    image.save(f"{dest_folder_name}/{file_name}.{file_ext}")  # Edited file name == source file name
-    # img_edited.save(f"{dest_folder_name}/{count}.{file_ext}")     # Edited file name == count
+def save_img_to_dest_folder(image: Image, dest_folder_name: str,
+                            file_name: str, file_ext: str) -> None:
+    image.save(f"{dest_folder_name}/{file_name}.{file_ext}")
 
 
 def main_thread(init_folder: str):
@@ -91,50 +98,44 @@ def main_thread(init_folder: str):
         file = item
         try:
             file_name, file_ext = get_file_name_and_ext(file)
-        except Exception:
+        except TypeError:
             print(f"{start_err_mgs} (wrong name or extension in file: \'{file.name}\')")
             count += 1
             continue
-        # print(f"{init_folder}/{file.name}")
+        file_path = f"{init_folder}/{file.name}"
         try:
-            file_path = f"{init_folder}/{file.name}"
             image = check_is_img(file_path, file_ext)
-            # print("*"*50, image.filename, file_name)
             width, height = image.size
         except (AttributeError, UnidentifiedImageError) as err:
-            print(f"{start_err_mgs} (file is not a proper image: \'{file.name}\' ({err}))")
+            print(f"{start_err_mgs} (file is not a proper image: "
+                  f"\'{file.name}\' ({err}))")
             count += 1
             continue
-        # print(f"[INFO] The file is genuine image: \'{image.filename.split('/')[-1]}\' (size: {image.size})")
         if check_is_screenshot(width, height, file_ext):
-            # print(f"[INFO] The file is screenshot: \'{image.filename.split('/')[-1]}\' (size: {image.size})")
             image = crop_bars(image)
-            # image.show()
-        # else:
-            # print(f"[INFO] The file is just image, not screenshot: \'{image.filename.split('/')[-1]}\' (size: {image.size})")
-        # print("before", file_name)
-        if img_bigger_than_default(width, height):
-            image = resize_img(image, width, height)
-        # print("after", file_name)
-        # print(f"=====\'{image}\' => size: {image.size}")
-        # print(image.__dir__())
-        save_img(image, dest_folder_path, file_name, file_ext, count)
+        if check_img_bigger_than_default(width, height):
+            image = resize_img_to_default(image, width, height)
+        save_img_to_dest_folder(image, dest_folder_path, file_name, file_ext)
         print(f"{start_good_msg} File is ready")
         count += 1
         count_handled += 1
 
-    dest_folder_images_amount = get_all_from_init_folder(dest_folder_path)["all_entities_amount"]
+    dest_folder_images_amount = \
+        get_all_from_init_folder(dest_folder_path)["all_entities_amount"]
     if dest_folder_images_amount:
         print(f"[Finish]\n"
-              f"\tSource folder: \'{init_folder}\'\n"
-              f"\tFiles in source folder: {count - 1}\n"
-              f"\tDestination folder: \'{dest_folder_path}\'\n"
-              f"\tSuccessfully processed images: {count_handled - 1}\n")
+              f"\t\tSource folder: \'{init_folder}\'\n"
+              f"\t\tFiles in source folder: {count - 1}\n"
+              f"\t\tDestination folder: \'{dest_folder_path}\'\n"
+              f"\t\tSuccessfully processed images: {count_handled - 1}\n")
     else:
-        print(f"No images were processed. Destination folder {dest_folder_path} will be deleted")
+        print(f"[Finish]\n"
+              f"No images were processed. "
+              f"Destination folder {dest_folder_path} will be deleted")
+        os.rmdir(dest_folder_path)
 
 
 if __name__ == "__main__":
-    init_folder = "/Users/Sasha/Documents/24"
+    init_folder = "/Users/Sasha/Documents/25"
     main_thread(init_folder)
 
